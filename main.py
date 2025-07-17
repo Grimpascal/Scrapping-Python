2cd8495f464ba58e7ec94a9b6373127533d3e42b
Grimpascal <armadalautmerah1993@gmail.com>
2025-07-17 22:29:35 +0700

    .

--- main.py
+++ main.py
@@ -5,50 +5,50 @@ from tabulate import tabulate
 import os
 import time
 
-# def utama():
-#     while True:
-#         os.system('cls')
-#         print('Ingin melihat data apa')
-#         print('''1. Data tim hockey
-# 2. Daftar project github''')
-#         pilihan = input('Masukkan pilihan : ')
-#         if pilihan == '1':
-#             hockey()
-#         elif pilihan == '2':
-#             github()
-#         else:
-#             print('Tidak ada dipilihan!')
-#             time.sleep(1)
-#             continue
+def utama():
+    while True:
+        os.system('cls')
+        print('Ingin melihat data apa')
+        print('''1. Data tim hockey
+2. Daftar project github''')
+        pilihan = input('Masukkan pilihan : ')
+        if pilihan == '1':
+            hockey()
+        elif pilihan == '2':
+            github()
+        else:
+            print('Tidak ada dipilihan!')
+            time.sleep(1)
+            continue
 
-# def hockey():
-#     os.system('cls')
-#     respon = requests.get('https://www.scrapethissite.com/pages/forms/?per_page=500')
-#     soup = BeautifulSoup(respon.text, "html.parser")
+def hockey():
+    os.system('cls')
+    respon = requests.get('https://www.scrapethissite.com/pages/forms/?per_page=500')
+    soup = BeautifulSoup(respon.text, "html.parser")
 
-#     mentah = soup.find_all("tr", class_="team")
+    mentah = soup.find_all("tr", class_="team")
 
-#     Hasil = []
-#     for block in mentah:
-#         namaElement = block.find('td', class_='name')
-#         namaTim = namaElement.get_text(strip=True)
+    Hasil = []
+    for block in mentah:
+        namaElement = block.find('td', class_='name')
+        namaTim = namaElement.get_text(strip=True)
 
-#         tahunElement = block.find('td', class_='year')
-#         tahunTim = tahunElement.get_text(strip=True)
+        tahunElement = block.find('td', class_='year')
+        tahunTim = tahunElement.get_text(strip=True)
 
-#         menangElement = block.find('td', class_='wins')
-#         menangTim = menangElement.get_text(strip=True)
+        menangElement = block.find('td', class_='wins')
+        menangTim = menangElement.get_text(strip=True)
 
-#         kalahElement = block.find('td', class_='losses')
-#         kalahTim = kalahElement.get_text(strip=True)
+        kalahElement = block.find('td', class_='losses')
+        kalahTim = kalahElement.get_text(strip=True)
 
-#         Hasil.append({'nama' : namaTim, 'tahun' : tahunTim, 'menang' : menangTim, 'kalah' : kalahTim})
+        Hasil.append({'nama' : namaTim, 'tahun' : tahunTim, 'menang' : menangTim, 'kalah' : kalahTim})
 
-#     data = pd.DataFrame(Hasil)
-#     data.index = data.index + 1
-#     print(tabulate(data, headers='keys', tablefmt='grid'))
-#     input('ENTER UNTUK KEMBALI >>>')
-#     utama()
+    data = pd.DataFrame(Hasil)
+    data.index = data.index + 1
+    print(tabulate(data, headers='keys', tablefmt='grid'))
+    input('ENTER UNTUK KEMBALI >>>')
+    utama()
 
 def github():
     os.system('cls')
@@ -74,5 +74,7 @@ def github():
     data = pd.DataFrame(hasil)
     data.index = data.index + 1
     print(tabulate(data, headers='keys', tablefmt='grid'))
+    input('TEKAN ENTER UNTUK KEMBALI>>>')
+    utama()
 
-github()
+utama()
